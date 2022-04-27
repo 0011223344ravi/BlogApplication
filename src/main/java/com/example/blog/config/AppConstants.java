e2f5238ebee8123858d23f1bf7250e57cf8c2c51
Ravi Gupta <0011223344ravi@gmail.com>
2022-04-27 10:46:26 +0530

    3rd commit

--- src/main/java/com/example/blog/config/AppConstants.java
+++ src/main/java/com/example/blog/config/AppConstants.java
@@ -3,10 +3,10 @@ package com.example.blog.config;
 public class AppConstants {
 	public static final String PAGE_NUMBER ="1";
 
-	public static final String PAGE_SIZE ="1";
+	public static final String PAGE_SIZE ="3";
 
-	public static final String SORT_BY ="1";
+	public static final String SORT_BY ="podtId";
 
-	public static final String SORT_DIR ="1";
+	public static final String SORT_DIR ="asc";
 
 }
